eec5e795f1d2231c6c98f76b939c479da4385329
mattgmdev <88032250+mattgmdev@users.noreply.github.com>
2022-06-21 13:52:56 +0200

    Updates

---
+++ precache-manifest.82bffe5a8d22b74ff32ffa8b67d38397.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad01e991a3b9ae8cbbae",
+    "url": "/robofriends-pwa/static/css/main.25d06bcb.chunk.css"
+  },
+  {
+    "revision": "ad01e991a3b9ae8cbbae",
+    "url": "/robofriends-pwa/static/js/main.ad01e991.chunk.js"
+  },
+  {
+    "revision": "373817c68105c979ba19",
+    "url": "/robofriends-pwa/static/css/1.6468f57a.chunk.css"
+  },
+  {
+    "revision": "373817c68105c979ba19",
+    "url": "/robofriends-pwa/static/js/1.373817c6.chunk.js"
+  },
+  {
+    "revision": "2581e180eec6a465185f",
+    "url": "/robofriends-pwa/static/js/runtime~main.2581e180.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-pwa/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "32e2b25512bae2ed812738193f71fbb5",
+    "url": "/robofriends-pwa/index.html"
+  }
+];
